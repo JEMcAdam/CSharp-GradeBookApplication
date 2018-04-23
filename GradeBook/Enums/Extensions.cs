f9a7c50a35232b0b85f1c910615c4f73a741a1d0
JEMcAdam <john.mcadam@scanmarqed.com>
2018-04-23 12:23:34 +0100

    Added Weighting to GetGPA

---
+++ GradeBook/Enums/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace GradeBook.Enums
+{
+    public static class Extensions
+    {
+        public static bool In<T>(this T val, params T[] values) where T : struct => values.Contains(val);
+    }
+}
